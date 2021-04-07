188b561dcc5daea174a0d6561094ac3c7b85c0fb
Bartek <Bartek@Example.com>
2021-04-07 15:14:20 +0200

    bombs_around method added

--- main.py
+++ main.py
@@ -91,33 +91,123 @@ import numpy as np
 import random
 
 
-def make_grid(size):
-    s = {"H":9, "W": 9, "Bombs": 10}
-    m = {"H":16, "W": 16, "Bombs": 40}
-    l = {"H":16, "W": 30, "Bombs": 99}
-    if size == "s": grid_meta = s
-    if size == "m": grid_meta = m
-    if size == "l": grid_meta = l
+def make_grid(grid_size):
 
-    arr = np.zeros((grid_meta["H"], grid_meta["W"]), dtype=int)
+    if grid_size == "s": grid_meta = {"H":9, "W": 9, "Bombs": 10}
+    if grid_size == "m": grid_meta = {"H":16, "W": 16, "Bombs": 40}
+    if grid_size == "l": grid_meta = {"H":16, "W": 30, "Bombs": 99}
 
-
-
+    field_status = np.zeros((grid_meta["H"], grid_meta["W"]), dtype=int)
+    bombs_map = np.zeros((grid_meta["H"], grid_meta["W"]), dtype=int)
     for i in range(grid_meta["Bombs"]):
         while True:
             a = random.randint(0, grid_meta["H"]-1)
             b = random.randint(0, grid_meta["W"]-1)
 
-            if arr[a][b] == 0:
-                arr[a][b] = 1
+            if bombs_map[a][b] == 0:
+                bombs_map[a][b] = 1
                 break
+            else: continue
+
+    bombs_around = check_how_many_bombs_is_around(bombs_map, grid_meta)
+
+    return bombs_map, field_status, bombs_around
+
+def check_how_many_bombs_is_around(bombs_map, grid_meta):
+    bombs_around = np.zeros((grid_meta["H"], grid_meta["W"]), dtype=int)
+
+    # print(bombs_around.shape)
+    # print(bombs_map.shape)
+    # print(grid_meta["H"])
+    # print(grid_meta["W"])
+    # for row in range(grid_meta["H"]):
+    #     print("row: ", row)
+    #     for colum in range(grid_meta["W"]):
+    #         print("col: ", colum)
+    #
+    #
+    for row in range(grid_meta["H"]):
+        for colum in range(grid_meta["W"]):
+
+
+
+
+            # field
+            if grid_meta["H"]-1 > row > 0 and grid_meta["W"]-1 > colum > 0: # center fields
+                bombs_around[row][colum] = bombs_map[row-1][colum-1]+\
+                                        bombs_map[row-1][colum]+\
+                                        bombs_map[row-1][colum+1]+\
+                                        bombs_map[row][colum-1]+\
+                                        bombs_map[row][colum+1]+\
+                                        bombs_map[row+1][colum-1]+\
+                                        bombs_map[row+1][colum]+\
+                                        bombs_map[row+1][colum+1]
+            #Corners
+            elif row == 0 and colum == 0: # left top corner
+                bombs_around[row][colum] = bombs_map[row][colum+1]+\
+                                        bombs_map[row+1][colum+1]+\
+                                        bombs_map[row+1][colum]
+
+            elif row == grid_meta["H"]-1 and colum == 0:  # bottom left corner
+                bombs_around[row][colum] = bombs_map[row-1][colum] + \
+                                        bombs_map[row-1][colum+1] + \
+                                        bombs_map[row][colum+1]
+
+            elif row == grid_meta["H"]-1 and colum == grid_meta["W"]-1:  # bottom right corner
+                bombs_around[row][colum] = bombs_map[row][colum-1] + \
+                                        bombs_map[row-1][colum-1] + \
+                                        bombs_map[row-1][colum]
+
+            elif row == 0 and colum == grid_meta["W"]-1: # right top corner
+                bombs_around[row][colum] = bombs_map[row][colum-1]+\
+                                        bombs_map[row+1][colum-1]+\
+                                        bombs_map[row+1][colum]
+
+            # Rows and Columns on sides
+            elif row == 0 and  grid_meta["W"]-1 > colum > 0 : # top row
+
+                bombs_around[row][colum] = bombs_map[row][colum+1]+\
+                                        bombs_map[row+1][colum+1]+\
+                                        bombs_map[row+1][colum]+\
+                                        bombs_map[row][colum-1] + \
+                                        bombs_map[row+1][colum-1]
+
+            elif 0 < row < grid_meta["H"]-1 and colum == 0: # left column
+                bombs_around[row][colum] = bombs_map[row-1][colum] + \
+                                        bombs_map[row-1][colum+1] + \
+                                        bombs_map[row][colum+1]+\
+                                        bombs_map[row+1][colum+1] + \
+                                        bombs_map[row+1][colum]
+
+            elif 0 < row < grid_meta["H"]-1 and colum == grid_meta["W"]-1:  # right column
+                bombs_around[row][colum] = bombs_map[row][colum-1]+\
+                                        bombs_map[row+1][colum-1]+\
+                                        bombs_map[row+1][colum]+\
+                                        bombs_map[row-1][colum-1] + \
+                                        bombs_map[row-1][colum]
+
+
+            elif row == grid_meta["H"]-1 and grid_meta["W"]-1 > colum > 0:  # bottom row
+                bombs_around[row][colum] = bombs_map[row-1][colum] + \
+                                        bombs_map[row-1][colum+1] + \
+                                        bombs_map[row][colum+1]+\
+                                        bombs_map[row][colum-1] + \
+                                        bombs_map[row-1][colum-1]
             else:
-                continue
-    return arr
+                print("Error!!!!1")
+                print("Operacja: ", row, colum)
+                print("row: ", row)
+                print("col: ", colum)
 
-print("Welcom in Sapper Game!")
-size = input("Choose size ('s', 'm' or 'l'): ")
-
-print(make_grid("l"))
+    return bombs_around
 
 
+
+# print("Welcom in Sapper Game!")
+# size = input("Choose size ('s', 'm' or 'l'): ")
+
+bombs_map, field_status, bombs_around = make_grid("s")
+
+print(bombs_map)
+print()
+print(bombs_around)
